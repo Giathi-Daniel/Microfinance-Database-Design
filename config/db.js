f2a99f36d8d28af0162e53299426cc79dc8ccc6d
Daniel Giathi <blacksharkchi@proton.me>
2024-10-28 22:37:20 +0300

    add error handling in my configuration file

--- config/db.js
+++ config/db.js
@@ -5,7 +5,12 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+})
+
+pool.getConnection((err) => {
+    if(err) throw err
 })
 
 module.exports = pool.promise()
